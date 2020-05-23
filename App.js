33344dec104d7ce5bf19323ac1763a86dd5b8d56
chiragsrvstv <chirag.srivastava@live.com>
2020-05-23 23:04:54 +0530

    firebase setup

--- App.js
+++ App.js
@@ -6,21 +6,42 @@ import LoginScreen from "./screen/LoginScreen";
 import WelcomeScreen from "./screen/WelcomeScreen";
 import ShowDetailScreen from "./screen/ShowDetailScreen";
 
+// Firestore setup
+import * as firebase from "firebase";
+import firebaseConfig from "../constants/firebaseConfig";
+
+// Optionally import the services that you want to use
+// import "firebase/auth";
+// import "firebase/database";
+// import "firebase/firestore";
+//import "firebase/functions";
+//import "firebase/storage";
+
+// Initializing Firebase
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+const dbCheck = (labname) => {
+  database.ref("labs/");
+};
+
 export default function App() {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
-  const [scannedData, setScannedData] = useState('');
+  const [scannedData, setScannedData] = useState("");
 
   const scannedDataHandler = (data) => {
     setScannedData(data);
-  }
+  };
 
   let content = <LoginScreen isUserLoggedIn={isUserLoggedIn} />;
 
   if (isUserLoggedIn) {
     content = <WelcomeScreen scannedDataHandler={scannedDataHandler} />;
   }
-  if(isUserLoggedIn && scannedData) {
-    content = <ShowDetailScreen data={scannedData} />
+  if (isUserLoggedIn && scannedData) {
+    dbCheck("a");
+    content = <ShowDetailScreen data={scannedData} />;
   }
 
   return (
@@ -34,6 +55,6 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: "white"
+    backgroundColor: "white",
   },
 });
